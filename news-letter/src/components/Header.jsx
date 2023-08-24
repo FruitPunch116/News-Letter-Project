1c8e929099a513e95d26d586f47ccef988293ef0
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 14:52:58 -0400

    navbar to routes

--- news-letter/src/components/Header.jsx
+++ news-letter/src/components/Header.jsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
+// import SearchBar from "./SearchBar";
 
-function Header () {
+/*function Header () {
     
 
     
     return (
         <header className="main-header light-mode">
-            <div className="navbar"><Link to={"/"}>Home</Link></div>
+            <div className="title">
+            <h1>Bulls & Bears magazzine</h1>
             <br />
-            <div className="navbar"><Link to={"/crypto"}>Crypto</Link></div>
+            <h2>Welcome to your volatile newsletter</h2>
+            </div>
+            
+            <div className="navbar home"><Link to={"/"}>Home</Link></div>
             <br />
-            <div className="navbar"><Link to={"/stocks"}>Markets</Link></div>
+            <div className="navbar crypto"><Link to={"/crypto"}>Crypto</Link></div>
+            <br />
+            <div className="navbar stocks"><Link to={"/stocks"}>Stocks</Link></div>
+            <SearchBar/>
+        </header>
+    )
+}*/
+
+function Header () {
+    return (
+        <header className="header">
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/crypto">Crypto</Link>
+                <Link to="/stocks">Stocks</Link>
+            </nav>
         </header>
     )
 }
