9b211b5f271539b9ec3fee871f7b53e6411be4eb
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 14:55:23 -0400

    create file to post and filter

---
+++ news-letter/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+
+function SearchBar () {
+
+    return (
+        <div className="search">
+            <input  className="search-bar" type="text" placeholder="Type anythting here"/>
+        </div>
+    )
+}
+
+export default SearchBar
