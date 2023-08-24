6cfb3e83a0fe5fde0efa003c2ad8501efbe9f426
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 14:51:53 -0400

    stocks section

--- news-letter/src/components/Stocks.jsx
+++ news-letter/src/components/Stocks.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react"
 
 function Stocks () {
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(()=> {
+        fetch("http://localhost:3000/Articles")
+        .then(r => r.json())
+        .then(data => setArticles(data))
+    },[])
+    
+    const stocksArticles = articles.filter(article => article.section === "Stocks")
 
     return (
         <div className="stocks-market">
             <h1>Stocks Market</h1>
+            <br />
+            <div className="stocks" articles={articles}>
+                {stocksArticles.map(article => <p>{article.body}</p>)}
+            </div>
         </div>
         
     )
